c3aa7aa1867710ce5255ff56a3f9c0251d33899b
AtulSikarwal <atulsikarwal5.com>
2022-07-14 12:49:18 +0530

    The code for List of Rides Using Id

---
+++ ListOfRidesUsingId/InvoiceGenerator/CabInvoiceCustomException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceGenerator
+{
+    
+        public class CabInvoiceCustomException : Exception
+        {
+        public enum ExceptionType
+        {
+            INVALID_RIDE_TYPE,
+            INVALID_DISTANCE,
+            INVALID_TIME,
+            NULL_RIDES,
+            INVALID_USER_ID,
+            INVALID_ID_DISTANCE,
+            INVALIDID_TIME
+        }
+
+        ExceptionType type;
+        public CabInvoiceCustomException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
+    
+
---
+++ ListOfRidesUsingId/InvoiceGenerator/InvoiceGenertor.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceGenerator
+{
+    public class InvoiceGenertor
+    {
+        public readonly int MINIMUM_COST_PER_KM;
+        public readonly int COST_PER_TIME;
+        public readonly int MINIMUM_FARE;
+        public RideType rideType;
+        public InvoiceGenertor(RideType rideType)
+        {
+            this.rideType = rideType;
+            if (RideType.NORMAL == rideType)
+            {
+                MINIMUM_COST_PER_KM = 10;
+                COST_PER_TIME = 1;
+                MINIMUM_FARE = 5;
+            }
+            else
+            {
+                MINIMUM_COST_PER_KM = 15;
+                COST_PER_TIME = 2;
+                MINIMUM_FARE = 20;
+            }
+
+        }
+
+        public double CalculateTotalFair(double distance, int time)
+        {
+            try
+            {
+                if (distance <= 0)
+                {
+                    throw new CabInvoiceCustomException(CabInvoiceCustomException.ExceptionType.INVALID_ID_DISTANCE, "distance is invalid");
+                }
+                else if (distance <= 0)
+                {
+                    throw new CabInvoiceCustomException(CabInvoiceCustomException.ExceptionType.INVALIDID_TIME, "time is valid");
+                }
+                else
+                {
+                    double totalFare = 0;
+                    totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+                    return Math.Max(totalFare, MINIMUM_FARE);
+                }
+
+            }
+            catch (CabInvoiceCustomException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+            {
+
+            }
+        }
+
+        public InvoiceSummery CalculateTotalFair(Ride[] rides)
+        {
+            double totalFair = 0;
+            foreach (Ride ride in rides)
+            {
+                totalFair += CalculateTotalFair(ride.distance, ride.time);
+            }
+
+            totalFair=Math.Max(totalFair, MINIMUM_FARE);
+            return new InvoiceSummery(rides.Length, totalFair);
+        }
+
+        public static double CalculateFare(double distance, int time)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
